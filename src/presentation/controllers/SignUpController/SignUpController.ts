09872bc38fecec184bb7fbe2684b1606ab94c2f5
Henrique J. Araujo <liderhenrique@gmail.com>
2025-03-31 17:09:14 -0300

    refactor: make SignUpController conforms to a protocols

--- src/presentation/controllers/SignUpController/SignUpController.ts
+++ src/presentation/controllers/SignUpController/SignUpController.ts
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helper'
+import type { Controller } from '../../protocols/controllers'
 import type { HttpRequest, HttpResponse } from '../../protocols/http'
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFieldds = [
       'name',
